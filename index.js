83b28ae7b5d9d3fcbbf65b76d381516a0cd7e326
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-01-09 17:03:45 +0100

    Add basic color randomizer and color picker without conditionals

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log(Hello World);
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+let userColor = randomColor({
+  luminosity: argv[2],
+  hue: argv[3],
+});
+console.log(userColor);
+console.log(
+  chalk.hex(userColor)(`
+###############################
+###############################
+###############################
+#####       ${userColor}       #####
+###############################
+###############################
+###############################
+`),
+);
+
+//console.log(argv[2], argv[3]);
+
+// let rCol = randomColor();
+// console.log(
+//   chalk.hex(rCol)(`
+// ###############################
+// ###############################
+// ###############################
+// #####       ${rCol}       #####
+// ###############################
+// ###############################
+// ###############################
+// `),
+// );
+
+//Overly complicated first attempt:
+// let rColHex = chalk.hex(rCol)(rCol);
+// let blockTop = chalk.hex(rCol)(
+//   '###############################\n###############################\n###############################\n#####      ',
+// );
+// let blockBottom = chalk.hex(rCol)(
+//   '      #####\n###############################\n###############################\n###############################',
+// );
+
+// console.log(blockTop, rColHex, blockBottom);
+
+// const cat = 'cat';
+// console.log
+//   '###############################\n###############################\n###############################\n My %s has %d ears',
+//   cat,
+//   2,
+// );
